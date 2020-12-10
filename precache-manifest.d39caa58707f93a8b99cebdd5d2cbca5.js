2310cec9440f58297299f9a185f53564e0c1ce70
gabrielsaruhashi <gabriel.saruhashi@gmail.com>
2020-12-10 14:25:53 -0300

    Updates

---
+++ precache-manifest.d39caa58707f93a8b99cebdd5d2cbca5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db38f1ef3c35d750cfc57fff936c316f",
+    "url": "/letters-toolbox-client/index.html"
+  },
+  {
+    "revision": "b1dac03755305b9c8d5c",
+    "url": "/letters-toolbox-client/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c5b692812b1987756d83",
+    "url": "/letters-toolbox-client/static/js/2.676aa2b8.chunk.js"
+  },
+  {
+    "revision": "ae9660678c62d2a4ec6353eb99bcef58",
+    "url": "/letters-toolbox-client/static/js/2.676aa2b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1dac03755305b9c8d5c",
+    "url": "/letters-toolbox-client/static/js/main.bf30ace8.chunk.js"
+  },
+  {
+    "revision": "24bcd99fda3d74099312",
+    "url": "/letters-toolbox-client/static/js/runtime-main.844400fb.js"
+  }
+]);
