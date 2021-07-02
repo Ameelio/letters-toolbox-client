4f5b177da57bd0254f8d0f7eb6f7f7ebaccd2044
gabrielsaruhashi <gabriel.saruhashi@gmail.com>
2021-07-02 10:31:55 -0300

    Updates

--- precache-manifest.475a88b35b83a814e9d696e0343b6722.js
+++ precache-manifest.475a88b35b83a814e9d696e0343b6722.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ce01a767f264b58a4e45973f98b58393",
+    "revision": "7947969bbc480ad4d622def4ef4c285c",
     "url": "/letters-toolbox-client/index.html"
   },
   {
-    "revision": "d083f288ab0081b10055",
+    "revision": "4c5f592127d893e7eb4e",
     "url": "/letters-toolbox-client/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/letters-toolbox-client/static/js/2.fc1eb5b6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d083f288ab0081b10055",
-    "url": "/letters-toolbox-client/static/js/main.054e9663.chunk.js"
+    "revision": "4c5f592127d893e7eb4e",
+    "url": "/letters-toolbox-client/static/js/main.b69179b5.chunk.js"
   },
   {
     "revision": "24bcd99fda3d74099312",
